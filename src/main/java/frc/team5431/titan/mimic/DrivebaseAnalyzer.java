855613ead903a5821d919868a8601e6a6c7dc418
micw1417 <wu355049@gmail.com>
2026-01-13 20:38:02 -0600

    First Commit! Advantage Kit Swerve Gen + Vision Template with our Titan Util

---
+++ src/main/java/frc/team5431/titan/mimic/DrivebaseAnalyzer.java
@@ -0,0 +1,27 @@
+package frc.team5431.titan.mimic;
+
+/**
+ * @author Ryan Hirasaki
+ */
+public interface DrivebaseAnalyzer {
+  // Get Speed Functions
+  public double getLeftSpeed();
+
+  public double getRightSpeed();
+
+  // Get Encoder Count Functions
+  public double getLeftEncoderCount();
+
+  public double getRightEncoderCount();
+
+  // Get Encoder Distance Functions
+  public double getLeftDistance();
+
+  public double getRightDistance();
+
+  // Get Heading
+  public float getHeading();
+
+  // Mimic stuff
+  public void setHome();
+}
