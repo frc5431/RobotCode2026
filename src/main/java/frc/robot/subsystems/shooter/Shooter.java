5a75e68b63bb8cf7f54c78624bc28aa4f4b8580e
AlbedoLinguine <yogevcr@gmail.com>
2026-01-16 19:10:47 -0600

    oopsies

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -5,8 +5,6 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.Constants;
-import frc.robot.Constants.IntakeConstants;
-import frc.robot.Constants.IntakeConstants.IntakeModes;
 import frc.robot.Constants.ShooterConstants;
 import frc.robot.Constants.ShooterConstants.ShooterModes;
 import frc.robot.Constants.ShooterConstants.ShooterState;
